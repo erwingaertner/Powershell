9219efae07926a96790641552d227bc743d5e781
erwingaertner <erwin.gaertner@gmail.com>
2023-02-19 20:46:02 +0100

    Test

---
+++ Copy-Exclude.ps1
@@ -0,0 +1,8 @@
+﻿
+[System.Data.Common.DBProviderFactories]::GetFactoryClasses() | select InvariantName
+
+
+$source = 'C:\Users\erwin\Documents\Test1'
+$dest = 'C:\Users\erwin\Documents\Test2'
+$exclude = @('*.pdf','*.txt')
+Get-ChildItem $source -Recurse -Exclude $exclude | Copy-Item -Destination {Join-Path $dest $_.FullName.Substring($source.length)}
