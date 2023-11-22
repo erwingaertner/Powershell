be690ca21cde0d4a9d83ba05d168565a22eca44c
erwin <76390132+erwingaertner@users.noreply.github.com>
2023-11-22 10:54:40 +0100

    Update Copy-Exclude.ps1

--- Copy-Exclude.ps1
+++ Copy-Exclude.ps1
@@ -1,8 +1,4 @@
-﻿
-[System.Data.Common.DBProviderFactories]::GetFactoryClasses() | select InvariantName
-
-
-$source = 'C:\Users\erwin\Documents\Test1'
-$dest = 'C:\Users\erwin\Documents\Test2'
+﻿$source = 'C:\Test1'
+$dest = 'C:\Test2'
 $exclude = @('*.pdf','*.txt')
 Get-ChildItem $source -Recurse -Exclude $exclude | Copy-Item -Destination {Join-Path $dest $_.FullName.Substring($source.length)}
