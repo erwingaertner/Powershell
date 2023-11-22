7ba455bf8bf02119ee0e592796bbaa83c18da110
Erwin Gärtner <erwin.gaertner@gmail.com>
2023-11-22 10:45:42 +0100

    Commit 1

--- Copy-Exclude.ps1
+++ Copy-Exclude.ps1
@@ -2,7 +2,7 @@
 [System.Data.Common.DBProviderFactories]::GetFactoryClasses() | Select-Object InvariantName
 
 
-$source = 'C:\Test1'
-$dest = 'C:\Test2'
+$source = 'C:\Test1\'
+$dest = 'C:\Test2\'
 $exclude = @('*.pdf','*.txt')
 Get-ChildItem $source -Recurse -Exclude $exclude | Copy-Item -Destination {Join-Path $dest $_.FullName.Substring($source.length)}
